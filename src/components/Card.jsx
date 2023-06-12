51187a46ab60c56b79fe124dd0858920b526271a
Martial Rahul Sharma <martialrahulsharma@gmail.com>
2023-06-12 23:57:35 +0530

    card update 2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,38 +9,51 @@ const Card = (props) => {
     <>
       <div className={classes.card}>
         <div className={classes.heading}>
-        <h2>{props.OutputHeading}</h2>
+          <h2>{props.OutputHeading}</h2>
         </div>
         <div className={classes.rowContainer}>
-        {props.lapeta !== 0 ? (
-          <div className={classes.rowContainer}>
-            <label>Lapeta(in sqr Ft):</label>
-            <span>{props.lapeta}</span>
+          {props.lapeta !== 0 ? (
+            <div className={classes.contentRowContainer}>
+              <label>Lapeta(in sqr Ft):</label>
+              <span>{props.lapeta}</span>
+            </div>
+          ) : (
+            ""
+          )}
+          {props.wallSqr !== "0" ? (
+            <div className={classes.contentRowContainer}>
+              <label>Wall(in sqr Ft):</label>
+              <span>{props.wallSqr}</span>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className={classes.rowContainer}>
+          <div className={classes.contentRowContainer}>
+            <label>Rs. per sqr ft:</label>
+            {props.perTileSqrFtPrice}
           </div>
-        ) : (
-          ""
-        )}
-        {props.wallSqr !== '0' ? <div className={classes.rowContainer}>
-          <label>Wall(in sqr Ft):</label>
-          <span>{props.wallSqr}</span>
-        </div> : ""}
-
+          <div className={classes.contentRowContainer}>
+            <label>Per box sqr ft:</label>
+            {props.perBoxSqrFt}
+          </div>
         </div>
         <div className={classes.rowContainer}>
-          <label>Rs. per sqr ft:</label>
-          {props.perTileSqrFtPrice}
-          <label>Per box sqr ft:</label>
-          {props.perBoxSqrFt}
+          <div className={classes.contentRowContainer}>
+            <label>Per tile sqr ft:</label>
+            {props.perTileSqrFt}
+          </div>
+          <div className={classes.contentRowContainer}>
+            <label>Total Boxes:</label>
+            {props.totalTilesBoxes}
+          </div>
         </div>
         <div className={classes.rowContainer}>
-          <label>Per tile sqr ft:</label>
-          {props.perTileSqrFt}
-          <label>Total Boxes:</label>
-          {props.totalTilesBoxes}
-        </div>
-        <div className={classes.rowContainer}>
+        <div className={classes.contentRowContainer}>
           <label>Total Price:</label>
-          <span>{props.totalPrice}/-</span>
+          <span>{props.totalPrice}</span>
+</div>
         </div>
       </div>
     </>
